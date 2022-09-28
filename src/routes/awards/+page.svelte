6a8a8d50d0c8e193c48ccca4dc4a99035b0ef3d1
Ballhog19 <102887518+Ballhog19@users.noreply.github.com>
2022-09-28 16:11:56 -0700

    First 3 managers

---
+++ src/routes/awards/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { Awards } from '$lib/components'
+	import LinearProgress from '@smui/linear-progress';
+
+    export let data;
+    const awardsData = data.awardsData;
+</script>
+
+<style>
+    .awards {
+        display: block;
+        margin: 30px auto;
+		width: 95%;
+		max-width: 1000px;
+		position: relative;
+		z-index: 1;
+		overflow-y: hidden;
+    }
+
+	.loading {
+		display: block;
+		width: 85%;
+		max-width: 500px;
+		margin: 80px auto;
+	}
+
+	.nothingYet {
+		display: block;
+		width: 85%;
+		max-width: 500px;
+		margin: 80px auto;
+		text-align: center;
+	}
+</style>
+
+<div class="awards">
+	{#await awardsData }
+		<div class="loading">
+			<p>Retrieving awards data...</p>
+			<LinearProgress indeterminate />
+		</div>
+	{:then {podiums, currentManagers} }
+		{#each podiums as podium}
+			<Awards {podium} {currentManagers} />
+		{:else}
+			<p class="nothingYet">No seasons have been completed yet, so no awards have been earned...</p>
+		{/each}
+	{:catch error}
+		<!-- promise was rejected -->
+		<p>Something went wrong: {error.message}</p>
+	{/await}
+</div>
---
+++ src/routes/transactions/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import LinearProgress from '@smui/linear-progress';
+	import { TransactionsPage } from '$lib/components'
+    import { waitForAll } from '$lib/utils/helper';
+
+    export let data;
+    const {show, query, page, playersData, transactionsData} = data;
+
+    let el, masterOffset;
+
+    const resize = (w) => {
+        masterOffset = el?.getBoundingClientRect() ? el?.getBoundingClientRect().left  : 0;
+    }
+	const perPage = 10;
+
+    $: resize(innerWidth);
+
+    let innerWidth;
+
+</script>
+
+<svelte:window bind:innerWidth={innerWidth} />
+
+<style>
+    #main {
+        position: relative;
+        z-index: 1;
+        display: block;
+        margin: 30px auto;
+		width: 95%;
+		max-width: 1000px;
+        overflow-y: hidden;
+    }
+
+	.loading {
+		display: block;
+		position: relative;
+		z-index: 1;
+		width: 85%;
+		max-width: 500px;
+		margin: 80px auto;
+	}
+</style>
+
+<div id="main" bind:this={el}>
+
+
+    {#await waitForAll(transactionsData, playersData)}
+        <div class="loading" >
+            <p>Loading league transactions...</p>
+            <LinearProgress indeterminate />
+        </div>
+    {:then [{transactions, currentManagers, stale}, playersInfo]}
+        <TransactionsPage {playersInfo} {stale} {transactions} {currentManagers} {masterOffset} {show} {query} queryPage={page} {perPage} postUpdate={true} />
+    {:catch error}
+        <p class="center">Something went wrong: {error.message}</p>
+    {/await}
+</div>
